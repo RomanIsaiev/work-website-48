5883d192459bd8dcb72558590a34ca97e4e1c7ef
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-06-23 22:41:59 +0300

    completed

---
+++ src/js/swiper.js
@@ -0,0 +1,17 @@
+// import Swiper JS
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper1 = new Swiper('.mySwiper1', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiper2 = new Swiper('.mySwiper2', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
